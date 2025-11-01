f907dcf49acbb8fbbd714c0587b6590033b5324e
Mykhailo Pyroh <mykhailopk@gmail.com>
2025-11-01 15:29:49 +0100

    ready

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { getImagesByQuery } from "./js/pixabay-api.js";
-import {
-  createGallery,
+import { getImagesByQuery } from "./js/pixabay-api.js"; 
+import { 
+  createGallery, 
   clearGallery,
   showLoader,
   hideLoader,
@@ -11,13 +11,20 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(".form");
 const input = form.querySelector("input[name='search-text']");
+const loadMoreBtn = document.querySelector(".load-more");
+
+let query = "";
+let page = 1;
+const perPage = 15;
+let totalHits = 0;
 
 form.addEventListener("submit", onSearch);
+loadMoreBtn.addEventListener("click", onLoadMore);
 
 async function onSearch(e) {
   e.preventDefault();
 
-  const query = input.value.trim();
+  query = input.value.trim();
   if (!query) {
     iziToast.warning({
       title: "Warning",
@@ -27,11 +34,14 @@ async function onSearch(e) {
     return;
   }
 
+  page = 1; 
   clearGallery();
+  hideLoadMoreButton();
   showLoader();
 
   try {
-    const data = await getImagesByQuery(query);
+    const data = await getImagesByQuery(query, page, perPage);
+    totalHits = data.totalHits;
 
     if (data.hits.length === 0) {
       iziToast.info({
@@ -44,6 +54,10 @@ async function onSearch(e) {
     }
 
     createGallery(data.hits);
+
+    if (totalHits > perPage) {
+      showLoadMoreButton();
+    }
   } catch (error) {
     iziToast.error({
       title: "Error",
@@ -54,4 +68,47 @@ async function onSearch(e) {
   } finally {
     hideLoader();
   }
+}
+
+async function onLoadMore() {
+  page += 1;
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page, perPage);
+    createGallery(data.hits);
+
+    const { height: cardHeight } = document
+      .querySelector(".gallery li")
+      .getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
+
+    if (page * perPage >= totalHits) {
+      hideLoadMoreButton();
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: "topRight",
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      title: "Error",
+      message: "Something went wrong! Please try again later.",
+      position: "topRight",
+    });
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+}
+
+function showLoadMoreButton() {
+  loadMoreBtn.classList.remove("is-hidden");
+}
+
+function hideLoadMoreButton() {
+  loadMoreBtn.classList.add("is-hidden");
 }
